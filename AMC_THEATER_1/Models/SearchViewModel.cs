745b5f8f43812318edc939952e61985b7c49f8e2
keyurzalavadiya24 <keyurzalavadiya24@gmail.com>
2025-03-13 16:10:16 +0530

    Added file

---
+++ AMC_THEATER_1/Models/SearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMC_THEATER_1.Models
+{
+	public class SearchViewModel
+	{
+        public int T_ID { get; set; }
+        public string T_NAME { get; set; }
+        public string T_CITY { get; set; }
+        public string T_ADDRESS { get; set; }
+        public string T_TENAMENT_NO { get; set; }
+        public string T_OWNER_NAME { get; set; }
+        public string REG_ID { get; set; }
+
+    }
+}
